ca0f77b5d8e01ed60370ef4226dc153678110ab3
Aryan Jagarwal <ajbaggar@gmail.com>
2022-01-23 12:18:37 +0530

    added searcbar properly

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -10,22 +10,30 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,    // added by me
       appBar: AppBar(
-        title: BrandName(),
+        backgroundColor: Colors.white,
+        title: brandName(),
         elevation: 0.0,
       ),
       body: Container(child: Column(
         children: <Widget>[
-          Row(children: <Widget>[
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Search"
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xfff5f8fd)
+            ),
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            child: Row(children: <Widget>[
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search Here"
+                  ),
                 ),
               ),
-            ),
-            Icon(Icons.search)
-          ],)
+              Icon(Icons.search)
+            ],),
+          )
         ],),),
     );
   }
--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-Widget BrandName(){
-  return Row(children: <Widget>[
+Widget brandName(){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
     Text("Wallpaper", style: TextStyle(color: Colors.black87),),
-    Text("Hub", style: TextStyle(color: Colors.blue),),
-  ],);
+    Text("Hub", style: TextStyle(color: Colors.blue)),
+    ],);
 }
